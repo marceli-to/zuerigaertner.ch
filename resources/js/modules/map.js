c4b49b840ebbee0a07fafae9e70eb5db6f59ec03
Marcel Stadelmann <m@marceli.to>
2023-05-17 13:34:14 +0200

    wip

--- resources/js/modules/map.js
+++ resources/js/modules/map.js
@@ -1,37 +1,62 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoibWFyY2VsaXRvb29vIiwiYSI6ImNrMHNsdmhwdjAzcjIzZ3BldTlqdWhnaWEifQ.EWZE383Tn4xBt0E5pSXh6Q';
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/marcelitoooo/ck16ms7m51nlo1cmwnqrbjuyq',
-    center: [8.497271271164212,47.38291388911902],
-    zoom: 12
-});
-map.addControl(new mapboxgl.NavigationControl());
-map.scrollZoom.disable();
+// Define a function to load the Mapbox script
+function loadMapboxScript() {
+  var script = document.createElement('script');
+  script.src = 'https://api.mapbox.com/mapbox-gl-js/v2.0.0/mapbox-gl.js';
+  script.onload = initializeMap; // Call the function to initialize the map once the script is loaded
+  document.head.appendChild(script);
+}
 
-var geojson = {
-  type: 'FeatureCollection',
-  features: [{
-    type: 'Feature',
-    geometry: {
-      type: 'Point',
-      coordinates: [8.497271271164212,47.38291388911902],
-    },
-    properties: {
-      title: 'Zürigärtner GmbH',
-      description: 'Zürigärtner GmbH'
-    }
-  }]
-};
+// Define a function to initialize the map
+function initializeMap() {
+  mapboxgl.accessToken = 'pk.eyJ1IjoibWFyY2VsaXRvb29vIiwiYSI6ImNrMHNsdmhwdjAzcjIzZ3BldTlqdWhnaWEifQ.EWZE383Tn4xBt0E5pSXh6Q';
+  var map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/marcelitoooo/ck16ms7m51nlo1cmwnqrbjuyq',
+      center: [8.497271271164212,47.38291388911902],
+      zoom: 12
+  });
+  map.addControl(new mapboxgl.NavigationControl());
+  map.scrollZoom.disable();
+  
+  var geojson = {
+    type: 'FeatureCollection',
+    features: [{
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [8.497271271164212,47.38291388911902],
+      },
+      properties: {
+        title: 'Zürigärtner GmbH',
+        description: 'Zürigärtner GmbH'
+      }
+    }]
+  };
+  
+  // add markers to map
+  geojson.features.forEach(function(marker) {
+  
+  // create a HTML element for each feature
+  var el = document.createElement('div');
+  el.className = 'marker';
+  
+  // make a marker for each feature and add to the map
+  new mapboxgl.Marker(el)
+    .setLngLat(marker.geometry.coordinates)
+    .addTo(map);
+  });
+}
 
-// add markers to map
-geojson.features.forEach(function(marker) {
+loadMapboxScript();
 
-// create a HTML element for each feature
-var el = document.createElement('div');
-el.className = 'marker';
+// // Add an event listener to trigger the Mapbox script loading when the user interacts with the page
+// window.addEventListener('scroll', function() {
+//   // Adjust the conditions for when to load the Mapbox script based on your requirements
+//   if (window.pageYOffset > 500) {
+//     loadMapboxScript();
+//     // Remove the event listener to avoid unnecessary script loading
+//     window.removeEventListener('scroll', arguments.callee);
+//   }
+// });
 
-// make a marker for each feature and add to the map
-new mapboxgl.Marker(el)
-  .setLngLat(marker.geometry.coordinates)
-  .addTo(map);
-});
+
